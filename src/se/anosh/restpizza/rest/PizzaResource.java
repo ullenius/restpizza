e7eec4bb8983079dd4f9bd0e99be8d8404f2d7da
Anosh D. Ullenius <anosh@anosh.se>
2019-02-05 15:17:56 +0100

    Interface, path basic stuff

--- src/se/anosh/restpizza/rest/PizzaResource.java
+++ src/se/anosh/restpizza/rest/PizzaResource.java
@@ -1,14 +1,33 @@
 /**
  * 
  * REST webservice implementation code
+ * 
+ * CRUD-functionality
+ * 
+ * 
  */
 
 package se.anosh.restpizza.rest;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+import se.anosh.restpizza.PizzaManagerServiceLocal;
 
 /**
  *
  * @author Anosh D. Ullenius <anosh@anosh.se>
  */
+@Stateless
+@Path("/pizza")
 public class PizzaResource {
     
+    
+            @Inject
+            PizzaManagerServiceLocal service;
+        
+        
+        
+        
+        
 }
